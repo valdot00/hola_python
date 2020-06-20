e47b11d4ba3005a19977d2f0595a16f545633550
Val <valdo_jopa@hotmail.com>
2020-06-20 01:00:19 -0500

    guardar

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,7 @@
 print("hola mundo")
 print("msg")
 print("test")
+
+cadena = "hola mundo"
+
+cadena
---
+++ testpython.py
@@ -0,0 +1,15 @@
+cadena = "hola mundo"
+
+print(cadena)
+
+num = 5
+num1= 1
+
+suma = num+num1
+
+print(suma)
+
+a=5
+b=4
+print(a*b)
+
