aeee72ab4669456b3db78c98b67a2fa5d3266ccc
Val <valdo_jopa@hotmail.com>
2020-06-20 01:15:43 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,41 @@
+#variables
+
+numero=5
+print(numero)
+
+cadena = "hola mundo"
+print(cadena)
+
+# error comun iniciar una variables con un numero 
+# sensible en mayusculas y minuculas
+
+numero=5
+Numero=6
+
+print(numero)
+print(Numero)
+
+#cadenas 
+
+cadena1="HOLA"
+cadena2="mundo"
+cadena3=cadena1+cadena2
+
+print(cadena3)
+
+num1=5
+num2=4
+suma=num1+num2
+
+print(suma)
+
+#tipo de variable
+
+tipodenumero=3
+
+print(type(tipodenumero))
+
+tipodecadena="hola"
+
+print(type(tipodecadena))
+
