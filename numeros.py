ef8ea488b289bd23f204c692dc521db2b8df49a7
Val <valdo_jopa@hotmail.com>
2020-06-21 00:58:44 -0500

    6 numeros

--- numeros.py
+++ numeros.py
@@ -1,2 +1,21 @@
 #numeros
 
+numero1 = 5
+
+print(numero1)
+
+#type
+
+print(type(numero1))
+
+#numero decimal 
+
+numerodesimal = 3.7
+
+print(type(numerodesimal))
+
+numero1=5
+numero2=3
+suma=numero1+numero2
+
+
