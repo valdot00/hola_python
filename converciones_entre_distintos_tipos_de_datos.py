0f3255894dddd3242ac5859495e87f6545380703
Val <valdo_jopa@hotmail.com>
2020-06-21 01:35:01 -0500

    7 converciones entre diferentes tipos de datos. actividad completada

---
+++ converciones_entre_distintos_tipos_de_datos.py
@@ -0,0 +1,31 @@
+#convercion entre diferentes tipos de datos
+
+numero=5
+cadena=str(numero)
+
+print(numero,type(numero))
+
+print(cadena,type(cadena))
+print("################################")
+###############################
+cadena='25'
+numero = int(cadena)
+
+print(numero,type(numero))
+
+numero2=4
+suma=numero+numero2
+
+print(suma)
+print("################################")
+################################
+cadena='25.7'
+numero = float(cadena)
+
+print(numero,type(numero))
+
+numero2=4
+suma=numero+numero2
+
+print(suma)
+print("################################")
