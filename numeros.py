02248782d14db484c7b4ab09defecf1cd86cf7fc
Val <valdo_jopa@hotmail.com>
2020-06-21 01:02:45 -0500

    tema 6 numeros actividad completada

---
+++ numeros.py
@@ -0,0 +1,23 @@
+#numeros
+
+numero1 = 5
+
+print(numero1)
+
+#type
+
+print(type(numero1))
+
+#numero decimal 
+
+numerodesimal = 3.7
+
+print(type(numerodesimal))
+
+numeros1=5
+numeros2=3.2
+suma=numeros1+numeros2
+
+print(type(suma))
+
+
