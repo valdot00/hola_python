ac148b7890266e2983b77432210307e8171df063
Val <valdo_jopa@hotmail.com>
2020-06-22 00:51:57 -0500

    8_ejercicio_completo

---
+++ 8_ejercicio.py
@@ -0,0 +1,19 @@
+#ejercicio 
+
+numero1=5
+
+print(type(numero1))
+
+numero2=6.5
+
+print(type(numero2))
+
+numero3=7
+
+print(type(numero3))
+
+suma=numero3+numero2+numero1
+
+print(suma,type(suma))
+
+print("actividad completa")
