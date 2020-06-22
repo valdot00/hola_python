332182da4db06f580ea53976b469172c176cacde
Val <valdo_jopa@hotmail.com>
2020-06-22 02:29:55 -0500

    10 cadena de texto

---
+++ 10_cadena_de_texto.py
@@ -0,0 +1,32 @@
+#10 cadena de texto
+
+cadena = "hola mundo"
+
+print(cadena)
+
+# H O L A   M U N D O
+# 0 1 2 3 4 5 6 7 8 9 
+
+print(cadena[5]) 
+
+#se puede imprimir regresivamente con numeros negativos
+print(cadena[-1]) 
+print(cadena[-10])
+
+#se puede imprimir una parte de una cadena 
+
+print(cadena[2:7])
+
+#SI NO COLOCAS el fin del limite tomara hasta el final de la cadena
+print(cadena[2:])
+
+print("-------------------")
+
+c1="hola"
+c2="mundo"
+c3=" "
+
+cadenas=c1+c3+c2
+
+print(cadenas)
+
