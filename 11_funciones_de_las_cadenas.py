780b8f8703b0502a1a020871e39bad4b8f794498
Val <valdo_jopa@hotmail.com>
2020-06-23 00:44:10 -0500

    11_funciones_de_las_cadenas: Actividad completada

---
+++ 11_funciones_de_las_cadenas.py
@@ -0,0 +1,34 @@
+#funciones de cadenas
+
+cadena = "hola mundo"
+
+print(cadena)
+print("--------------------------")
+#len(cadena) muestra la longitud de la cadena
+
+print(len(cadena))
+print("--------------------------")
+#cadena.upper() trasforma tu cadena en mayusculas
+
+print(cadena.upper())
+
+#para cambiar la cadena a mayusculas
+
+print("--------------------------")
+cadena=cadena.upper()
+
+print(cadena)
+
+print("--------------------------")
+print(cadena.lower())#para mostrarlo en minusculas
+
+print("--------------------------")
+#para dividir las palabras de una cadena
+print(cadena.split())
+
+print("--------------------------")
+cadena2="uva,pera,manzana,naranja"
+
+print(cadena2)
+print("--------------------------")
+print(cadena2.split(','))#para dividir una cadena por un caracter
