fc4f58db23e0916214851fba9d4420be78a73462
Val <valdo_jopa@hotmail.com>
2020-06-25 01:02:46 -0500

    14 ejercicio: actividad completada

---
+++ 14_ejercicio.py
@@ -0,0 +1,11 @@
+#14 ejercicio
+#crea una variables "cadena" 
+# que contiene el texto "esto es un texto de ejemplo"
+#segun la posicion de cada letra en la cadena calcular que valores 
+#(x,y) hay que poner para selecionar solo la cadena [x:y]="texto"
+
+cadena="esto es un texto de ejemplo"
+
+print("-----------")
+print(cadena[11:17])
+print("-----------")
