c700081dab4002b5788aeca4be95a4d5ec79d5ad
Val <valdo_jopa@hotmail.com>
2020-06-26 00:41:27 -0500

    16 ejercicio 2

---
+++ 16_ejercicio_2.py
@@ -0,0 +1,23 @@
+#ejercicio
+#crear una variable "cadena" que contiene el texto "esto es un texto de ejemplo"
+#crear una variable "longitud" que contiene la longitud (numero de caracteres) de la variable "cadena"
+#crear una variable "mayusculas" que contiene la variable(cadena) en mayusculas
+# crear un variable "resultado" que concatene la variable "mayusculas" y la variable "resultado" convertida a string
+#  
+
+
+cadena="esto es un texto de ejemplo"
+
+print(cadena)
+
+longitud=len(cadena)
+
+print(longitud)
+
+mayusculas=cadena.upper()
+
+print(mayusculas)
+
+resultado=str(mayusculas) 
+
+print(resultado)
