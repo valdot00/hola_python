56bb4fd089473dad0a712db73d42bbc7827445d1
Val <valdo_jopa@hotmail.com>
2020-06-27 02:43:45 -0500

    18 ejercicio 3 : actividad completada

---
+++ 18_ejercicio_3.py
@@ -0,0 +1,15 @@
+#ejercicio
+#crear una variable "numero1" que contiene el valor 5
+#crear una variable "numero2" que contiene el valor 8
+#crear una variable "suma" que contiene la suma de los numeros anteriores
+#imprimir  por pantalla el resultado utilizando la funcion print
+
+numero1=5
+
+numero2=8
+
+suma=numero1+numero2
+
+#.format(variable,variable2,)
+
+print("el resultado de {} + {} = {} ".format(numero2,numero1,suma))
