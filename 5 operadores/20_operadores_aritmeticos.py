ea4ab96ecff51cd8cabc2977df1769ac70cc6bbe
Val <valdo_jopa@hotmail.com>
2020-06-28 03:00:07 -0500

    20 operadores arimeticos : acticidad completada

---
+++ 5 operadores/20_operadores_aritmeticos.py
@@ -0,0 +1,37 @@
+#operadores arimeticos + , - , * , / , % , ** , //
+
+numero1=10
+numero2=5
+suma=numero1+numero2
+
+print(suma)
+
+resta=numero1-numero2
+
+print(resta)
+
+multipliacacion=numero2*numero1
+
+print(multipliacacion)
+
+division=numero1/numero2
+
+print(division)
+
+#operador para el resto %
+numero1=5
+numero2=2
+
+resto=numero1%numero2
+
+print(resto)
+
+cociente=numero1//numero2
+
+print(cociente)
+
+exponente = 2**3
+
+print(exponente)
+
+print("-----------")
