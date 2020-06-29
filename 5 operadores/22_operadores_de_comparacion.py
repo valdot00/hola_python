c3cdad38385a14fd60bd81f8745d67c8de42e08d
Val <valdo_jopa@hotmail.com>
2020-06-29 01:50:05 -0500

    22 operadores de comparacion : actividad completada

---
+++ 5 operadores/22_operadores_de_comparacion.py
@@ -0,0 +1,40 @@
+#operadores de comparacion (==,!=,>,<,>=,<=)
+print("--------------")
+numero1=5
+numero2=3
+
+print(numero1==numero2)
+
+cadena1="hola"
+cadena2="hola"
+
+print(cadena1==cadena2)
+
+#print("\n")
+#print("escribe algo")
+#input(cadena1)
+
+if(cadena1=="hola"):
+    print("dijo hola")
+
+# != distinto de
+
+numero1 = 3
+numero2 = 5
+
+print(numero1 != numero2)
+
+print(numero1 > numero2)
+
+print(numero1 < numero2)
+
+print(numero1 >= numero2)
+
+print(numero1 <= numero2)
+
+print("--------------")
+
+
+
+
+
