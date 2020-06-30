bbaaec42285b80c35fbfdf9d823b819fdb0b20d2
Val <valdo_jopa@hotmail.com>
2020-06-30 00:47:06 -0500

    23 operadores logicos

---
+++ 5 operadores/23_operadores_logicos.py
@@ -0,0 +1,29 @@
+#operadores logicos (and,or,not)
+
+numero1=10
+numero2=5
+numero3=7
+numero4=8
+
+print(numero1>numero2)
+
+print(numero3<numero4)
+
+print((numero1>numero2)and(numero3<numero4))
+
+print(numero3>numero4)
+
+print((numero1>numero2)and(numero3>numero4))
+
+#operador or
+
+print((numero1>numero2)or(numero3>numero4))
+
+#operador not
+
+print(numero3>numero4)
+
+print(not(numero3>numero4))
+
+
+
