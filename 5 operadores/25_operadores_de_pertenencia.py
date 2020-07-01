4d7255b41cb9cfb4e0b3ecfd9be76dad47d256b2
Val <valdo_jopa@hotmail.com>
2020-07-01 17:53:32 -0500

    25 operadores de pertenencia : actividad completada

--- 5 operadores/25_operadores_de_pertenencia.py
+++ 5 operadores/25_operadores_de_pertenencia.py
@@ -7,7 +7,20 @@ x=(frutas2 in frutas)
 
 print(x)
 
-print(frutas2 in frutas)
+print(frutas2 in frutas)#pera si esta en el array
+
+print("----------")
+#not in
+
+x=(frutas2 not in frutas)
+
+print(x)
+
+print(frutas2 not in frutas) 
+
+frutas3="melocoton"
+
+print(frutas3 not in frutas) #melocoton no esta en el array
 
 
 
