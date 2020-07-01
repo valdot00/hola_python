3928034a39926734d8a086f5634b6c0b6a7e4407
Val <valdo_jopa@hotmail.com>
2020-07-01 17:36:14 -0500

    24 operadores de identidad

---
+++ 5 operadores/24_operadores_de_identidad.py
@@ -0,0 +1,21 @@
+#operadores de identidad (is, is not)
+
+frutas=["manzana","pera"]
+frutas2=["manzana","pera"]
+frutas3=frutas
+
+x=(frutas3 is frutas)
+
+print(x)
+
+print(frutas3 is frutas)
+
+frutas3.append("naranja") #para agregar una elemento a un arreglo
+
+print(frutas3)
+
+#is not 
+
+frutas3 is not frutas
+
+print(frutas3 is not frutas)
