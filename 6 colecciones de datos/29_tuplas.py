fbe6dff87dce12612e94598e74b45275faf42c0a
Val <valdo_jopa@hotmail.com>
2020-07-02 14:09:44 -0500

    29 tuplas : actividad completada

---
+++ 6 colecciones de datos/29_tuplas.py
@@ -0,0 +1,21 @@
+#29 tuplas
+
+tuplas_colores = ("rojo","verde","amarillo")
+
+print(tuplas_colores)
+
+print("-----------------------------")
+
+for color in tuplas_colores:
+    print(color)
+
+print("-----------------------------")
+
+print(tuplas_colores[2])
+
+print("-----------------------------")
+
+#en las tuplas no se pueden borrar o añadir elementos
+# las tuplas no se pueden modificar
+
+print(len(tuplas_colores))
