67a07da58ac52dbc44a71aadbe7f6756487edbb0
Val <valdo_jopa@hotmail.com>
2020-07-02 14:06:34 -0500

    29 tuplas

---
+++ 6 colecciones de datos/29_tuplas.py
@@ -0,0 +1,15 @@
+#29 tuplas
+
+tuplas_colores = ("rojo","verde","amarillo")
+
+print(tuplas_colores)
+
+for color in tuplas_colores:
+    print(color)
+
+print(tuplas_colores[2])
+
+#en las tuplas no se pueden borrar o añadir elementos
+# las tuplas no se pueden modificar
+
+print(len(tuplas_colores))
