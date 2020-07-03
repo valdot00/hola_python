a759bd3da78055ca69d168593c5dc728803e44ee
Val <valdo_jopa@hotmail.com>
2020-07-03 00:07:22 -0500

    30 conjuntos : actividad completada

---
+++ 6 colecciones de datos/30_conjuntos.py
@@ -0,0 +1,32 @@
+#30 conjuntos
+
+conjunto_colores = {"rojo","verde","azul"}
+
+print(conjunto_colores)
+
+print("-------------------------")
+
+for color in conjunto_colores:
+    print(color)
+
+#print(conjunto_colores[0]) no funciona por que los conjuntos no tienen orden
+
+print("-------------------------")
+
+print(len(conjunto_colores))   
+
+print("-------------------------")
+
+conjunto_colores.add("negro")
+
+print(conjunto_colores)
+
+print("-------------------------")
+
+conjunto_colores.remove("verde")
+
+print(conjunto_colores)
+
+print("-------------------------")
+
+
