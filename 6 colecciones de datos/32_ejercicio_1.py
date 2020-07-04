5b46656b4d4b2742c5e71fb64c1bab2d37e62d9d
Val <valdo_jopa@hotmail.com>
2020-07-04 13:47:49 -0500

    32 ejercicio 1

---
+++ 6 colecciones de datos/32_ejercicio_1.py
@@ -0,0 +1,17 @@
+#32 ejercicio 1
+
+#dada la siguiente lista=[1,2,5,25,33,56,75,21,56,89,43,13,62,24]
+#mostrar mediante el metodo "print" y el operador in, si el numero 21 esta en la lista
+
+lista=[1,2,5,25,33,56,75,21,56,89,43,13,62,24]
+
+print(lista)
+
+numero=21
+
+if(numero in lista):
+    print("el {} si esta en la lista".format(numero))
+else:
+    print("el {} NO esta en la lista".format(numero))
+
+    
