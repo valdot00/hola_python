11486f472cf2f44c72681de0d9ea0a50f713250c
Val <valdo_jopa@hotmail.com>
2020-07-05 03:16:57 -0500

    34 condiciones if else

---
+++ 7 bucles y condiciones/34_condiciones_if_else.py
@@ -0,0 +1,67 @@
+#34 condiciones if else
+a=8
+b=4
+
+print(a>b)
+
+print(a<b)
+
+if(a>b):
+    print("a es mayor que b")
+
+if(a<b):
+    print("a es mayor que b")    
+
+a=8
+b=4
+c=2
+d=6
+
+if(a>c)and(b<d):
+    print("la primera exprecion es correcta")
+
+if(a>c)and(b>d):
+    print("la primera exprecion es correcta")    
+
+if(a>c)and(b>d):
+    print("la primera exprecion es correcta")
+else:
+    print("la primera exprecion NO es correcta")    
+
+a=8
+b=4
+
+if(a>b):
+    print("a es mayor que b")
+elif (a==b):
+    print("a es igual a b")
+else:
+    print("ninguna exprecion anterior es correcta")
+
+a=8
+b=8
+
+if(a>b):
+    print("a es mayor que b")
+elif (a==b):
+    print("a es igual a b")
+else:
+    print("ninguna exprecion anterior es correcta")    
+
+a=8
+b=10
+
+if(a>b):
+    print("a es mayor que b")
+elif (a==b):
+    print("a es igual a b")
+else:
+    print("ninguna exprecion anterior es correcta")      
+
+
+
+
+
+
+
+
