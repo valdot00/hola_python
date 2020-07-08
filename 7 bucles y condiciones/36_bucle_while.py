efa5e0d341defa39637017de23b94cd0d90cb360
Val <valdo_jopa@hotmail.com>
2020-07-08 02:06:08 -0500

    36 bucle while : actividad completa

---
+++ 7 bucles y condiciones/36_bucle_while.py
@@ -0,0 +1,34 @@
+#36 bucle while
+
+valor = 1
+fin = 10
+
+while (valor < fin):
+       print(valor)
+       valor=valor+1
+
+print("-")
+
+valor = 1
+fin = 10
+
+while (valor < fin):
+       print(valor)
+       valor=valor+1
+       if(valor==5):
+           break
+
+print("-")        
+
+#continue
+
+valor=1
+fin=10
+
+while(valor<fin):
+    valor=valor+1
+    if(valor==6):
+        continue
+    print(valor)
+
+print("-")    
