b4dde987ff7e2b0b09992a9ace170f155e3b12f7
Val <valdo_jopa@hotmail.com>
2020-07-14 00:41:01 -0500

    43 funciones lambda : actividad completada

---
+++ 8 clases objetos y funciones/43_funciones_lambda.py
@@ -0,0 +1,13 @@
+#43 funciones lambda
+
+resultado = lambda numero : numero + 30
+
+print(resultado(20))
+
+print("---")
+
+resultado2 = lambda numero1,numero2 : numero1 + numero2
+
+print(resultado2(8,2))
+
+print("---")
