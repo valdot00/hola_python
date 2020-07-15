bccea1e054da825394f683fe0e3e742259837bfc
Val <valdo_jopa@hotmail.com>
2020-07-15 00:51:30 -0500

    46 ejercicio 2 : actividad completada

---
+++ 8 clases objetos y funciones/46_ejercicio_2.py
@@ -0,0 +1,11 @@
+#46 ejercicio 2
+
+#crear una funcion lambda que calcule la media de 3 notas
+
+media=lambda nota1,nota2,nota3 : (nota1+nota2+nota3)/3 
+
+print(media(4,6,5))
+
+print(media(3,4.5,7.6))
+
+print(media(6,7,9))
