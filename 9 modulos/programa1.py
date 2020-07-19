7c9fc53b34fa3d7f46211af143cfa4571e375c84
Val <valdo_jopa@hotmail.com>
2020-07-18 21:27:57 -0500

    50 ejercicio

---
+++ 9 modulos/programa1.py
@@ -0,0 +1,11 @@
+#programa1.py
+
+import ejercicio_50
+
+coche1 =ejercicio_50.coche("fiat","blanco","diesel","1.5")
+
+print(coche1.mostrar())
+
+media = ejercicio_50.media(3,8,7)
+
+print("nuestra nota media es{}".format(media))
