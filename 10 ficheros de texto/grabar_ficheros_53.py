5cc5cc9acf2b196baee761938bfe99eedbe5de2b
Val <valdo_jopa@hotmail.com>
2020-07-24 01:12:23 -0500

    53 grabar ficheros : actividad completada

---
+++ 10 ficheros de texto/grabar_ficheros_53.py
@@ -0,0 +1,11 @@
+#53 grabar ficheros
+
+# grabar un fichero de texto
+
+fichero=open("fichero_para_grabar.txt","wt")
+
+texto_del_fichero="hola esta es la linea que vamos a grabar en el fichero de texto"
+
+fichero.write(texto_del_fichero)
+
+fichero.close()
