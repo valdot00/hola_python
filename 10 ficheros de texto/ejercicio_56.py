cbf99bc3bf53d10c7971063da62e3db4e8880ff2
Val <valdo_jopa@hotmail.com>
2020-07-28 01:11:24 -0500

    56 ejercicio 1 :

---
+++ 10 ficheros de texto/ejercicio_56.py
@@ -0,0 +1,21 @@
+#56 ejercicio
+    
+ #crear el modulo "moduloficheros.py"
+ #crear una clase "fichero"
+ #crear la funcion "leer_fichero" para leer de un fichero de texto
+ #crear la funcion "grabar_fichero" para crear un fichero de texto
+ #crear la funcion "incluir_fichero" para incluir datos al final de un fichero de texto
+
+#crar un programa "programa1.py"
+ 
+ #crear el objeto "fichero" de la clase "fiochero" del modulo 
+ #"moduloficheros.py"
+ #utilizar el metodo "grabar_fichero" del objeto "fichero" para crear un
+ #nuevo fichero de texto
+ #utilizar el metodo "incluir_fichero" para incorporar mas datos al final
+ #del fichero
+ #utilizar el metodo "leer_fichero" para ver todo el contenido del fichero
+
+ #ejecutar el programa "programa.py" desde la terminal de comandos
+
+ 
