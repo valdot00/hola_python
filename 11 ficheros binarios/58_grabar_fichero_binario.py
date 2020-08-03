fe9583eb230dd0be6669ee5622545a0fcdfe8768
Val <valdo_jopa@hotmail.com>
2020-08-02 22:48:51 -0500

    58 grabar fichero binario : actividad completada

--- 11 ficheros binarios/58_grabar_fichero_binario.py
+++ 11 ficheros binarios/58_grabar_fichero_binario.py
@@ -1,1 +1,11 @@
 #58 grabar fichero binario 
+
+import pickle
+
+lista_colores = ["azul","verde","rojo","amarillo"]
+
+fichero = open("fichero_colores.pckl","wb")
+
+pickle.dump(lista_colores,fichero)
+
+fichero.close()
