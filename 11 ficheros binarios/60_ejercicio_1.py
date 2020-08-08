1d7a82d4c06fad9e49edcef25a72c51bd6c3c676
Val <valdo_jopa@hotmail.com>
2020-08-07 21:25:21 -0500

    60 ejercicio 1 :

---
+++ 11 ficheros binarios/60_ejercicio_1.py
@@ -0,0 +1,15 @@
+#60 ejercicio 1
+
+#crear el diccionario "frutas"
+
+frutas={"manzana":"apple","naranja":"orange","platano":"banana","limon":"lemon"}
+
+#gravar esta estructura de datos "frutas" en un 
+# fichero binario "fichero.pckl"
+#ya que en un fichero de texto, solo se guadan 
+# caracteristicas, pero no se
+#puede guardar estas estructuras
+
+#repupera esta escrutura de datos del fichero "fichero.pckl"
+#verifica que sigue siendo un diccionario, 
+# ejecutando el metodo .values()
