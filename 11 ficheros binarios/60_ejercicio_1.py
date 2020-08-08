5c57917a582d4fb0a52be2495fec4e68bfe50d86
Val <valdo_jopa@hotmail.com>
2020-08-08 02:05:29 -0500

    60 ejercicio 1 : actividad completada

--- 11 ficheros binarios/60_ejercicio_1.py
+++ 11 ficheros binarios/60_ejercicio_1.py
@@ -1,15 +1,35 @@
 #60 ejercicio 1
 
 #crear el diccionario "frutas"
+#gravar esta estructura de datos "frutas" en un fichero binario
+#"fichero.pckl"
+#ya que en un fichero de texto, solo se guadan caracteristicas,
+#pero no se
+#puede guardar estas estructuras
+#repupera esta escrutura de datos del fichero "fichero.pckl"
+#verifica que sigue siendo un diccionario,
+#ejecutando el metodo .values()
 
 frutas={"manzana":"apple","naranja":"orange","platano":"banana","limon":"lemon"}
 
-#gravar esta estructura de datos "frutas" en un 
-# fichero binario "fichero.pckl"
-#ya que en un fichero de texto, solo se guadan 
-# caracteristicas, pero no se
-#puede guardar estas estructuras
+print(frutas.values())
 
-#repupera esta escrutura de datos del fichero "fichero.pckl"
-#verifica que sigue siendo un diccionario, 
-# ejecutando el metodo .values()
+#dict_values(['apple','orange','banana','lemon'])
+
+fichero = open("fichero1.pckl","wb")
+
+import pickle
+
+pickle.dump(frutas,fichero)
+
+fichero.close()
+
+fichero2 = open("fichero1.pckl","rb")
+
+datos = pickle.load(fichero2)
+
+print(datos)
+
+datos.values()
+
+print(datos.values())
