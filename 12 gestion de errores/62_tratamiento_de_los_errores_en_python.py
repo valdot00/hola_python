429438979427e8a24bd63352f6d999439b637969
Val <valdo_jopa@hotmail.com>
2020-08-12 00:45:53 -0500

    62 tratamiento de los errores en python :

--- 12 gestion de errores/62_tratamiento_de_los_errores_en_python.py
+++ 12 gestion de errores/62_tratamiento_de_los_errores_en_python.py
@@ -1,1 +1,15 @@
 #62 tratamiento de los errores en python
+
+#try...except...else...finally
+
+#numero1 = 5
+#numero2 = 0
+#division = numero1/numero2
+
+try:
+    numero1=5
+    numero2=0
+    division=numero1/numero2
+    print(division)
+except:
+    print("ha habido un error")    
