3df2364f3e35780087b7b9a4c436ee611dfabffa
Val <valdo_jopa@hotmail.com>
2020-08-12 00:56:28 -0500

    62 tratamiento de los errores en python : actividad completada

--- 12 gestion de errores/62_tratamiento_de_los_errores_en_python.py
+++ 12 gestion de errores/62_tratamiento_de_los_errores_en_python.py
@@ -12,4 +12,34 @@ try:
     division=numero1/numero2
     print(division)
 except:
-    print("ha habido un error")    
+    print("ha habido un error")   
+
+#zeroDivisionError
+
+try:
+    numero1=5
+    numero2=2
+    division=numero1/numero2
+    print(division)
+except zeroDivisionError:
+    print("Error, division entre cero")
+except:
+    print("ha habido un error")
+else:
+    print("la division funciono correctamente")  
+
+#finally
+
+try:
+    numero1=5
+    numero2=2
+    division=numero1/numero2
+    print(division)
+except zeroDivisionError:
+    print("Error, division entre cero")
+except:
+    print("ha habido un error")
+else:
+    print("la division funciono correctamente")  
+finally:
+    print("esta prueba del try se ha acabado")    
