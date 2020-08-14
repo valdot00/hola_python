dcf6ad941af20632a0223e15bac04ac3a51e2ed6
Val <valdo_jopa@hotmail.com>
2020-08-14 01:51:18 -0500

    63 ejercicio 1 :

--- 12 gestion de errores/63_ejercicio_1.py
+++ 12 gestion de errores/63_ejercicio_1.py
@@ -1,1 +1,9 @@
 #63 ejercicio 1
+# crear una funcion "operacion" que dados 3 numeroes, divida el primer
+#numero entre la resta de los otros dos numeroes
+
+#utiliza la funcion creada con los numeroes 5,4,2
+#utiliza la funcion creada con los numeroes 6,3,3
+
+#utiliza el bloque try... except para controlar cuaklquier 
+#posible error
