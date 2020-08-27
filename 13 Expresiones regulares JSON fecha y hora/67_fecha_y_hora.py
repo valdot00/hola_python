8a62b432785b7759a8da1d0098918860012901f5
Val <valdo_jopa@hotmail.com>
2020-08-27 00:39:10 -0500

    67 fecha y hora :

---
+++ 13 Expresiones regulares JSON fecha y hora/67_fecha_y_hora.py
@@ -0,0 +1,20 @@
+#67 fecha y hora
+
+from datetime import datetime
+
+fechayhora = datetime.now()
+
+print(fechayhora)
+
+año = fechayhora.year
+mes = fechayhora.month
+dia = fechayhora.day
+
+#hora
+
+hora = fechayhora.hour
+minutos = fechayhora.minute
+segundos = fechayhora.second
+microsegundos = fechayhora.microsecond
+
+print("la hora es {}:{}:{}".format(hora,minutos,segundos))
