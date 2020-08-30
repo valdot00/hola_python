f5158d3d9d39eecc910277f68b5f86ca7cf76967
Val <valdo_jopa@hotmail.com>
2020-08-30 02:22:06 -0500

    70 base de datos : actividad completada

--- 14 base de datos/70_crear_base_de_datos.py
+++ 14 base de datos/70_crear_base_de_datos.py
@@ -1,1 +1,11 @@
 #70 crear base de datos
+
+#SQLite - sistema de gestion de base de datos relacionales
+
+# sqlitebrowser.org
+
+import sqlite3
+
+conexion = sqlite3.connect("basededatos.db")
+
+conexion.close()
