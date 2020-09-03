900a15d454fc8a001d017edf68c0f10144c61947
Val <valdo_jopa@hotmail.com>
2020-09-03 12:13:47 -0500

    73 insertar varias filas a la vez : actividad completada

--- 14 base de datos/73_insertar_varias_filas_a_la_vez.py
+++ 14 base de datos/73_insertar_varias_filas_a_la_vez.py
@@ -1,2 +1,15 @@
 #73 insertar varias filas a la vez
 
+import sqlite3
+
+conexion = sqlite3.connect("basededatos1.db")
+
+cursor = conexion.cursor()
+
+lista_personas = [('pedro','rodigez','perez',26), ('maria','lopez','gomez',46), ('jorge','pelayo','arellano',28)]
+
+cursor.executemany("INSERT INTO PERSONAS VALUES (?,?,?,?)", lista_personas)
+
+conexion.commit()
+
+conexion.close()
