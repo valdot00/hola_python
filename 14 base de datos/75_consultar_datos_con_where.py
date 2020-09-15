0353a42305a8e31ccd49e47ff98689266dbe18c9
Val <valdo_jopa@hotmail.com>
2020-09-14 21:20:01 -0500

    75 consultar datos con where : actividad completada

---
+++ 14 base de datos/75_consultar_datos_con_where.py
@@ -0,0 +1,16 @@
+#75 consultar datos con where.
+
+import sqlite3
+
+conexion = sqlite3.connect("basededatos1.db")
+
+cursor = conexion.cursor()
+
+cursor.execute("SELECT * FROM PERSONAS WHERE edad < 40")
+
+personas_seleccionadas = cursor.fetchall()
+
+for persona in personas_seleccionadas:
+    print(persona)
+
+conexion.close()    
