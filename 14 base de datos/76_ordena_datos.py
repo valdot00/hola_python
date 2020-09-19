b6c2d133f7757f915edfc25bc43bd91b3ab50cf1
Val <valdo_jopa@hotmail.com>
2020-09-19 12:20:06 -0500

    76 ordenar datos : actividad completada

--- 14 base de datos/76_ordena_datos.py
+++ 14 base de datos/76_ordena_datos.py
@@ -2,15 +2,15 @@
 
 import sqlite3
 
- conexion = sqlite3.connect("basededatos1.db")
+conexion = sqlite3.connect("basededatos1.db")
 
- cursor = conexion.cursor()
+cursor = conexion.cursor()
 
- cursor.execute("SELECT * FROM PERSONAS ORDER BY edad")
+cursor.execute("SELECT * FROM PERSONAS ORDER BY edad")
 
- lista_personas_ordenada = cursor.fetchall()
+lista_personas_ordenada = cursor.fetchall()
 
- for persona in lista_personas_ordenada:
+for persona in lista_personas_ordenada:
      print(persona)
 
 conexion.close()      
