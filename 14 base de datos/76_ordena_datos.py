fab7fb853d1081061045eb6e39018fb94e88ecab
Val <valdo_jopa@hotmail.com>
2020-09-19 02:10:06 -0500

    76 ordena datos :

--- 14 base de datos/76_ordena_datos.py
+++ 14 base de datos/76_ordena_datos.py
@@ -1,2 +1,16 @@
 #76 ordena datos
 
+import sqlite3
+
+ conexion = sqlite3.connect("basededatos1.db")
+
+ cursor = conexion.cursor()
+
+ cursor.execute("SELECT * FROM PERSONAS ORDER BY edad")
+
+ lista_personas_ordenada = cursor.fetchall()
+
+ for persona in lista_personas_ordenada:
+     print(persona)
+
+conexion.close()      
