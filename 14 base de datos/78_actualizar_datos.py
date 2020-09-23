083a35449dbc4d9c421b62abcf805572401fb55c
Val <valdo_jopa@hotmail.com>
2020-09-23 01:01:09 -0500

    78 actualizar datos :

---
+++ 14 base de datos/78_actualizar_datos.py
@@ -0,0 +1,12 @@
+#78 actualizar datos
+
+import sqlite3
+
+conexion = sqlite3.connect("basededatos1.db")
+
+cursor = conexion.cursor()
+
+cursor.execute("UPDATE PERSONAS SET edad = 30 WHERE nombre ='antonio'")
+
+conexion.commit()
+conexion.close()
