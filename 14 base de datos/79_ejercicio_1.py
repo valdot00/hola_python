b43fa654805f6b633370702ef042b393b057f165
Val <valdo_jopa@hotmail.com>
2020-09-30 10:50:51 -0500

    79 ejercicio 1 :

---
+++ 14 base de datos/79_ejercicio_1.py
@@ -0,0 +1,16 @@
+#ejercicio
+
+#crear una base de datos "basededatos2.db"
+#crear una tabla "productos" con 3 campos
+       #id: identificador del procuto de tipo numerico
+       #nombre: nombre del producto de tipo texto
+       #precio: precio del producto de tipo numerico
+
+#insertar 3 ´productos en la tabla "productos"
+# 1,"impresora",300
+# 2,"raton",20
+# 3,"ordenador",600
+
+#consultar los productos de la tabla "productos"
+# cerrar la base de datos "basededatos.db" 
+       
