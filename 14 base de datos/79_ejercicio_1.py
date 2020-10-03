c803cbf957ad2239b1b721793d61e4ac80678490
Val <valdo_jopa@hotmail.com>
2020-10-03 10:35:05 -0500

    79 ejercicio 1 : actividad completada

--- 14 base de datos/79_ejercicio_1.py
+++ 14 base de datos/79_ejercicio_1.py
@@ -15,5 +15,26 @@
 # cerrar la base de datos "basededatos.db"
 
 import sqlite3
- 
-       
+
+conexion = sqlite3.connect("basededatos2.db")
+
+cursor = conexion.cursor()
+cursor.execute("CREATE TABLE PRODUCTOS (ID INTEGER, NOMBRE TEXT, PRECIO INTEGER)")
+
+conexion.commit()
+
+cursor.execute("INSERT INTO PRODUCTOS VALUES (1,'IMPRESORA',300)")
+conexion.commit()
+
+cursor.execute("INSERT INTO PRODUCTOS VALUES (2,'RATON',20)")
+conexion.commit()
+
+cursor.execute("INSERT INTO PRODUCTOS VALUES (1,'OREDENADOR',600)")
+conexion.commit()
+
+cursor.execute("SELECT * FROM PRODUCTOS")
+productos = cursor.fetchall()
+for producto in productos:
+    print(producto)
+
+conexion.close()
