212cf7c32f9381ca9f55e96b932bfd7319f78710
Val <valdo_jopa@hotmail.com>
2020-10-30 20:54:08 -0600

    88 componente radiobutton : actividad completada

--- 15 tkinter/89_componente_radiobutton.py
+++ 15 tkinter/89_componente_radiobutton.py
@@ -5,18 +5,18 @@ raiz.title("radiobutton")
 
 #creamos los radiobutton
 
-#def seleccionar():
- #   print("la opcion selecionada es {}".format(opcion.get()))
+def seleccionar():
+    print("la opcion selecionada es {}".format(opcion.get()))
 
-#opcion = tkinter.IntVar()    
+opcion = tkinter.IntVar()    
 
-#radiobutton1 = tkinter.Radiobutton(raiz,text="Opcion 1",variable=opcion, value=1, command=seleccionar)
-#radiobutton1.pack()
+button1 = tkinter.Radiobutton(raiz,text="Opcion 1",variable=opcion, value=1, command=seleccionar)
+button1.pack()
 
-#radiobutton2 = tkinter.Radiobutton(raiz,text="Opcion 2",variable=opcion, value=2, command=seleccionar)
-#radiobutton2.pack()
+button2 = tkinter.Radiobutton(raiz,text="Opcion 2",variable=opcion, value=2, command=seleccionar)
+button2.pack()
 
-#radiobutton3 = tkinter.Radiobutton(raiz,text="Opcion 3",varianble=opcion, value=3, command=seleccionar)
-#radiobutton3.pack()
+button3 = tkinter.Radiobutton(raiz,text="Opcion 3",variable=opcion, value=3, command=seleccionar)
+button3.pack()
 
 raiz.mainloop()
