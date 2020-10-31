7047a50e070d6b882712dd6e89c46c62141620b2
Val <valdo_jopa@hotmail.com>
2020-10-31 09:59:15 -0600

    90 componente checkbutton : actividad completada

--- 15 tkinter/90_checkbutton.py
+++ 15 tkinter/90_checkbutton.py
@@ -5,4 +5,18 @@ import tkinter
 raiz = tkinter.Tk()
 raiz.title("checkbutton")
 
+#creamos nuestro checkbutton
+
+def verificar():
+    valor = check1.get()
+    if (valor == 1):
+        print("el check esta activado")
+    else:
+        print("el check esta desactivado")    
+
+check1 = tkinter.IntVar()
+
+boton1 = tkinter.Checkbutton(raiz, text="Opcion 1", variable=check1, onvalue=1, offvalue=0, command=verificar)
+boton1.pack()
+
 raiz.mainloop()
