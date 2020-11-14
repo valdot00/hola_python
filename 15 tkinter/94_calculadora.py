db8e28b30a769c7622945b2d3f827157e889387b
Val <valdo_jopa@hotmail.com>
2020-11-13 20:23:07 -0600

    94 calculadora :

--- 15 tkinter/94_calculadora.py
+++ 15 tkinter/94_calculadora.py
@@ -6,14 +6,25 @@ ventana.geometry("400x600")
 ventana.resizable(False,False)
 ventana.configure(background="gray")
 
+# Funciones
+
+operacion = ""
+resultado = StringVar()
+
+def borrar():
+    global operacion
+    global resultado
+    operacion = ""
+    pantalla.delete(0,END)
+
 # display de los resultados
 
 pantalla = Entry(ventana,font=("arial",20,"bold"), borderwidth=2)
 pantalla.grid(row=0,column=0,columnspan=3,pady=10,padx=5)
 
-# BOTON DE REINICIAR operacion
+# boton de reiniciar operacion
 
-boton_reset = Button(ventana,text="AC",width=8,height=2)
+boton_reset = Button(ventana,text="AC",width=8,height=2,command=lambda:borrar())
 boton_reset.grid(row=0,column=3,pady=10)
 
 ventana.mainloop()
