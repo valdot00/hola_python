fa5046415c64c60958ce70d43e2899ef59608af9
Val <valdo_jopa@hotmail.com>
2020-11-13 20:12:42 -0600

    linea 5 geometri 350x600 modificado por 400x600

--- 15 tkinter/94_calculadora.py
+++ 15 tkinter/94_calculadora.py
@@ -1,2 +1,19 @@
 from tkinter import *
 
+ventana = Tk()
+ventana.title("calculadora")
+ventana.geometry("400x600")
+ventana.resizable(False,False)
+ventana.configure(background="gray")
+
+# display de los resultados
+
+pantalla = Entry(ventana,font=("arial",20,"bold"), borderwidth=2)
+pantalla.grid(row=0,column=0,columnspan=3,pady=10,padx=5)
+
+# BOTON DE REINICIAR operacion
+
+boton_reset = Button(ventana,text="AC",width=8,height=2)
+boton_reset.grid(row=0,column=3,pady=10)
+
+ventana.mainloop()
