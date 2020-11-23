9bd1f35c1a2ea28dd1797d817bfcca0afc36bf72
Val <valdo_jopa@hotmail.com>
2020-11-23 10:41:39 -0600

    96_pydoc.py : actividad completada

--- 17 documentacion AU/96_pydoc.py
+++ 17 documentacion AU/96_pydoc.py
@@ -1,2 +1,16 @@
 # pydoc - generar documentacion automatica desde
 # la consola o terminal de comandos
+
+class saludos:
+    """
+    esta clase tendra dos funciones buenos_dias y adios
+    ambas fuciones reciviran como parametro un nombre
+    """
+    def buenos_dias(self,nombre):
+        """ esta funcion sirve para decir buenos dias a una persona """
+        print("buenos dias {}".format(nombre))
+
+    def adios(self,nombre):
+        """esta fucion dice adios a una persona"""
+        print("adios {}".format(nombre))    
+
