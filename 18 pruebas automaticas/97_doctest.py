397a649e0d53fdd24fa4ef8e433a5fd8863792c9
Val <valdo_jopa@hotmail.com>
2020-12-01 13:44:48 -0600

    97 doctest : actividad completada

--- 18 pruebas automaticas/97_doctest.py
+++ 18 pruebas automaticas/97_doctest.py
@@ -1,1 +1,24 @@
 # Doctest - generar pruebas dentro de la documentacion
+
+def sumar(numero1,numero2):
+    """
+    esto es la documentacion de esta funcion 
+    recibe dos numeros como parametros y devuelve su suma
+
+    >>> sumar(4,3)
+    7
+
+    >>> sumar(5,4)
+    9
+
+    >>> sumar(1,3)
+    7
+
+    """
+    return numero1 + numero2
+
+resultado = sumar(2,4)
+print(resultado)
+
+import doctest
+doctest.testmod()
