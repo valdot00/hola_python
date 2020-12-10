e4b30449640e0a2243e4fc0996560f3d2031a95c
Val <valdo_jopa@hotmail.com>
2020-12-10 12:22:30 -0600

    99 funciones generadas : actividad completada

--- 19 funciones avanzadas/99_funciones_generadas.py
+++ 19 funciones avanzadas/99_funciones_generadas.py
@@ -1,1 +1,17 @@
 #funciones generadoras
+
+range(0,11)
+
+print(range(0,11))
+
+for numero in range(0,11):
+    print(numero)
+
+def pares (maximo):
+    for numero in range(maximo):
+        if (numero % 2 == 0):
+            yield numero
+
+maximo = 11
+for numero in pares(maximo):
+    print(numero)                
