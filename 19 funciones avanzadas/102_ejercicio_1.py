765ea65a763fc10e595a4286663f710abf54cd3a
Val <valdo_jopa@hotmail.com>
2020-12-20 02:49:03 -0600

    102 ejercicio 1 :

---
+++ 19 funciones avanzadas/102_ejercicio_1.py
@@ -0,0 +1,9 @@
+# ejercicio
+
+#crear la funcion "primos" que sera una funcion generadora 
+# de numeros primos entre 0 y el 100
+#esta es la lista de numeros primos entre 0 y 100
+#numeros_primos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+
+#utiliza la funcion generadora para mostrar por pantalla numeros primos menores de 50
+
