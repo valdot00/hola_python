f9fee03bc1c467bccbdde5b8c12f66909a29a2c9
Val <valdo_jopa@hotmail.com>
2020-12-27 19:08:46 -0600

    103 ejercicio 2 :

---
+++ 19 funciones avanzadas/103_ejercicio_2.py
@@ -0,0 +1,6 @@
+#ejercicio 
+
+#apartir de la lista "numeros" que contiene numeros del 1 al 10
+#obtener mediante "filter" una lsita denominada "pares" con los 
+#numeros pares de la lista "numeros"
+
