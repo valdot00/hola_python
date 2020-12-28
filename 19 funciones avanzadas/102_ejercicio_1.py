5aabcaa3c3959d78bc46daaced7710947faabc9b
Val <valdo_jopa@hotmail.com>
2020-12-27 19:05:13 -0600

    102 ejercicio 1 : actividad completada

--- 19 funciones avanzadas/102_ejercicio_1.py
+++ 19 funciones avanzadas/102_ejercicio_1.py
@@ -7,3 +7,17 @@
 
 #utiliza la funcion generadora para mostrar por pantalla numeros primos menores de 50
 
+numeros_primos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+
+def primos(maximo):
+    for numero in range(maximo):
+        if (numero in numeros_primos):
+            yield numero
+        if (numero > 100):
+            break
+
+maximo = 50
+
+for numero in primos(maximo):
+    print(numero)
+                
