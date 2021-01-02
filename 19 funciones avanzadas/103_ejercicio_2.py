9518d84cc4b4d024eb157540def61fb31a58537b
Val <valdo_jopa@hotmail.com>
2021-01-01 20:43:24 -0600

    103 ejercicio 2 : actividad completada

--- 19 funciones avanzadas/103_ejercicio_2.py
+++ 19 funciones avanzadas/103_ejercicio_2.py
@@ -4,3 +4,20 @@
 #obtener mediante "filter" una lsita denominada "pares" con los 
 #numeros pares de la lista "numeros"
 
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+def par(numero):
+    if(numero % 2)==0:
+        return True
+    else:
+        return False
+
+print(par(5))
+
+print(par(10)) 
+
+resultado = filter(par,numeros)
+
+pares = list(resultado)
+
+print(pares)
