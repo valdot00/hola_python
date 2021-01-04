10ecd46a5987c3594ebfff5b89cbc538fa852001
Val <valdo_jopa@hotmail.com>
2021-01-03 22:49:37 -0600

    104 ejercicio 3 : actividad completada

--- 19 funciones avanzadas/104_ejercicio_3.py
+++ 19 funciones avanzadas/104_ejercicio_3.py
@@ -3,4 +3,18 @@
 #a partir de una lista de numeros del 1 al 10,
 #optener una nueva lista con todos los elementos incrementados en 10 unidades
 
-def 
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+print(numeros)
+
+def incrementar(numero):
+    resultado = numero + 10
+    return resultado
+
+print(incrementar(5))
+
+resultado = map(incrementar,numeros)
+
+numeros2 = list(resultado)
+
+print(numeros2)
