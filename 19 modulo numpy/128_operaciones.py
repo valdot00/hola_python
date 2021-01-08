9d8070effbb81aedcdc5d084895845099109da0e
Val <valdo_jopa@hotmail.com>
2021-01-08 11:40:10 -0600

    128 operaciones : actividad completa

---
+++ 19 modulo numpy/128_operaciones.py
@@ -0,0 +1,36 @@
+# operaciones con arraya (+,-,*,/,**)
+
+import numpy as np
+
+array1 = np.array([1,2,3,4])
+
+print(array1)
+
+array1+4
+
+print(array1)
+
+array1=array1+4
+
+print(array1)
+
+lista1 = [1,2,3,4]
+lista2 = [5,6,7,8]
+lista_doble = (lista1,lista2)
+array_doble = np.array(lista_doble)
+
+array([1,2,3,4],[5,6,7,8])
+
+print(array_doble.dtype)
+
+print(array_doble.shape)
+
+arry([1,2,3,4],[5,6,7,8])
+
+array_doble+5
+
+array_doble/1
+
+array_doble**2
+
+array_doble = array_doble + 4
