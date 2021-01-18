c8c7ae9ecab683dd47d3f08f1928b86da37eef7e
Val <valdo_jopa@hotmail.com>
2021-01-18 12:12:20 -0600

    132 funciones con arrays : actividad completada

---
+++ 19 modulo numpy/132_funciones_con_arrays.py
@@ -0,0 +1,22 @@
+# funciones en arrays
+
+import numpy as np
+
+array = np.arange(5)
+
+print(array)
+
+print(np.sqrt(array))
+
+print(np.random.rand(5))
+
+lista = [5,6,7,8]
+array2 = np.array(lista)
+
+print(array)
+
+print(array2)
+
+print(np.add(array,array2))
+
+print(np.maximum(array,array2)) #muestra el valor amsximo de lso dos arrays
