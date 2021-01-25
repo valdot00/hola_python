fbdd0951841bb3501e22cca6a09c32e0da226b90
Val <valdo_jopa@hotmail.com>
2021-01-25 11:32:25 -0600

    138 ejercicio 3 : actividad completada

--- 19 modulo numpy/138_ejercicio_3.py
+++ 19 modulo numpy/138_ejercicio_3.py
@@ -5,3 +5,28 @@
 #crear otra lista con los ultimos 10 valores de la lista inicial 
 #crear un bucle que recorra esta ultima lista de valores finales
 
+import numpy as np 
+
+listal = np.arange(0,30)
+
+print(listal)
+
+principio = listal[0:10]
+
+print(principio)
+
+print(principio.ahape)
+
+final = listal[20:30]
+
+print(final)
+
+final = listal[-10:]
+
+print(final)
+
+for valor in final:
+    print(valor)
+    
+
+
