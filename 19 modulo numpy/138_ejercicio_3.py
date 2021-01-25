fcadf44db9fc8c65c067aaef33fb40bb2797c837
Val <valdo_jopa@hotmail.com>
2021-01-25 01:30:28 -0600

    138 ejercicio 3 :

---
+++ 19 modulo numpy/138_ejercicio_3.py
@@ -0,0 +1,7 @@
+#ejercicio 3
+
+#crear una lista con los valores numericos del 0 al 30 
+#crear otra lista con los primeros 10 valores de la lista inicial
+#crear otra lista con los ultimos 10 valores de la lista inicial 
+#crear un bucle que recorra esta ultima lista de valores finales
+
