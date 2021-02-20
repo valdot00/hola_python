a0222dbdd850750b48e3ad2be762e50a5fcb82d6
Val <valdo_jopa@hotmail.com>
2021-02-19 21:16:16 -0600

    150 ejercicio 1 :

---
+++ 20 modulo pandas/150_ejercicio_1.py
@@ -0,0 +1,10 @@
+# ejercicio
+
+# tenemos 3 clases "clase1","clase2","clase3"
+# vamos a generar un numero aletorio de alumnnos por clase
+# para obtener un numero aleatorio utilizaremos
+# np.random.randint(minimo,maximo,numero)
+
+# crear una serie de clases y alumnos
+# utilizar el indice de la serie creada para saber el numero 
+# de alumnos de la clase2
