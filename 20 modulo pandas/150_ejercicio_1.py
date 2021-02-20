96775ce5392c5e596f0f078eb2fa2c5b2b1dc028
Val <valdo_jopa@hotmail.com>
2021-02-20 12:11:08 -0600

    150 ejercicio 1 : actividad completa

--- 20 modulo pandas/150_ejercicio_1.py
+++ 20 modulo pandas/150_ejercicio_1.py
@@ -8,3 +8,19 @@
 # crear una serie de clases y alumnos
 # utilizar el indice de la serie creada para saber el numero 
 # de alumnos de la clase2
+
+import pandas as pd
+import numpy as np
+
+minimo = 10
+maximo = 40
+numero = 3
+alumnos = np.random.randint(minimo,maximo,numero)
+
+print(alumnos)
+
+clases = ['clase1','clases2','clase3']
+
+serie = pd.Series(alumnos,index=clases)
+
+print(serie)
