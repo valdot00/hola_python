6821bb3b2de5d08f02bda9854448f221e34ac509
Val <valdo_jopa@hotmail.com>
2021-03-01 19:47:35 -0600

    154 HTML : actividad completada

--- 21 HTML Y EXCEL/154_html.py
+++ 21 HTML Y EXCEL/154_html.py
@@ -1,1 +1,27 @@
 # HTML con python
+
+import pandas as pd
+
+url = 'https://es.wikipedia.org/wiki/Anexo:Finales_de_la_Copa_Mundial_de_F%C3%BAtbol'
+
+print(pd.io.html.read_html(url))
+
+dataframe = pd.io.html.read_html(url)
+
+print(dataframe)
+
+dataframe_futbol = dataframe[0]
+
+print(dataframe_futbol)
+
+print(dataframe_futbol.loc[0])
+
+print(dict(dataframe_futbol.loc[0]))
+
+dataframe_futbol = dataframe_futbol.rename(columns = dict(dataframe_futbol.loc[0]))
+
+print(dataframe_futbol)
+
+dataframe_futbol = dataframe_futbol.drop('Nntas',axis=1)
+
+print(dataframe_futbol)
