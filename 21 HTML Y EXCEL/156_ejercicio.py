4ad15543e1ab36fe69c21062f91e9a6ffd6697f7
Val <valdo_jopa@hotmail.com>
2021-03-09 11:15:02 -0600

    156 ejercicio :

---
+++ 21 HTML Y EXCEL/156_ejercicio.py
@@ -0,0 +1,8 @@
+# ejercicio 
+
+#lerr el fichero adjunto "poblacion.xlsx" y cargar los datos en un dataframe
+#con esos datos, visualizar cual es la ciudad mas popular en america
+
+#leer el fichero adjunto "poblacion.csv" y cargar los datos en un dataframe
+#con esos datos, visualizar cual es la ciudad mas poblada en africa
+
