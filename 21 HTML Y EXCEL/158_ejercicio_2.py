4f164de2bc90afa2781e1d1abcd66ebccd9d6aab
Val <valdo_jopa@hotmail.com>
2021-03-11 11:28:35 -0600

    158 ejercicio 2 :

---
+++ 21 HTML Y EXCEL/158_ejercicio_2.py
@@ -0,0 +1,5 @@
+# ejercicio 2
+
+#obtener la tabla de paises de la paguina 'https://es.wikipedia.org/wiki/anexo:pa%c3%aDses'
+#limpiar los datos lo necesario para que los nombres de la columnas sean correctos.
+
