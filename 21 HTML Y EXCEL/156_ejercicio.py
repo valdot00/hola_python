6c45f2c41be8043cc38a30ff6d7a24ad34d979d4
Val <valdo_jopa@hotmail.com>
2021-03-10 23:01:00 -0600

    156 ejercicio : actividad completada

--- 21 HTML Y EXCEL/156_ejercicio.py
+++ 21 HTML Y EXCEL/156_ejercicio.py
@@ -6,3 +6,20 @@
 #leer el fichero adjunto "poblacion.csv" y cargar los datos en un dataframe
 #con esos datos, visualizar cual es la ciudad mas poblada en africa
 
+import PANDAS as pd
+
+from pandas.core.frame import DataFrame
+
+excel = pd.ExcelFile('\Users\PC\Desktop\jorge_oswaldo_pelayo_arellano\python\21 HTML Y EXCEL\poblacion.xlsx')
+
+dataframe = excel.parse('Hoja 1')
+
+print(dataframe)
+
+print(DataFrame['ciudad mas poblada'][3])
+
+fichero_csv = pd.read_csv('C:\Users\PC\Desktop\jorge_oswaldo_pelayo_arellano\python\21 HTML Y EXCEL\poblacion.csv')
+
+print(fichero_csv)
+
+print(fichero_csv['ciudad mas poblada'][1])
