02e17f27e34f0d09cf90024bb53f3cbe0fe1cb2f
Val <valdo_jopa@hotmail.com>
2021-03-17 00:20:21 -0600

    154 ejercicio 2 : actividad completada

--- 21 HTML Y EXCEL/158_ejercicio_2.py
+++ 21 HTML Y EXCEL/158_ejercicio_2.py
@@ -3,3 +3,18 @@
 #obtener la tabla de paises de la paguina 'https://es.wikipedia.org/wiki/anexo:pa%c3%aDses'
 #limpiar los datos lo necesario para que los nombres de la columnas sean correctos.
 
+import pandas as pd
+
+excel = pd.ExcelFile('\Users\PC\Desktop\jorge_oswaldo_pelayo_arellano\python\21 HTML Y EXCEL\poblacin.xls')
+
+dataframe = excel.parse('Hoja 1')
+
+print(dataframe)
+
+print(dataframe['ciudad mas poblada'][3])
+
+fichero_csv = pd.read_csv('\Users\PC\Desktop\jorge_oswaldo_pelayo_arellano\python\21 HTML Y EXCEL\poblacin.csv')
+
+print(fichero_csv)
+
+print(fichero_csv['ciudad mas poblada'][1])
