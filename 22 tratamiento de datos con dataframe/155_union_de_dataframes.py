447189322c677a98c93d898c95dfe4d4cf7c00b2
Val <valdo_jopa@hotmail.com>
2021-03-19 12:01:53 -0600

    155 union de dataframes : actividad completada

--- 22 tratamiento de datos con dataframe/155_union_de_dataframes.py
+++ 22 tratamiento de datos con dataframe/155_union_de_dataframes.py
@@ -1,1 +1,30 @@
 # union de dataframe
+
+import pandas as pd
+
+dataframe1 = pd.DataFrame({'c1':['1','2','3'],'clave':['a','b','c']})
+
+print(dataframe1)
+
+dataframe2 = pd.DataFrame({'c2':['4','5','6'],'clave':['c','b','e']})
+
+print(dataframe2)
+
+dataframe3 = pd.DataFrame.merge(dataframe1,dataframe2,on='clave')
+
+print(dataframe3)
+
+dataframe4 = pd.DataFrame.merge(dataframe1,dataframe2,on='clave',how='left')
+
+print(dataframe4)
+
+dataframe4 = pd.DataFrame.merge(dataframe1,dataframe2,on='clave',how='right')
+
+print(dataframe4)
+
+print(dataframe2)
+
+dataframe4 = pd.DataFrame.merge(dataframe1,dataframe2,on='clave',how='outer')
+
+print(dataframe4)
+
