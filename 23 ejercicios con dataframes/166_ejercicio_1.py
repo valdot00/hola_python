a62afa8eaac4fac0733b47f1952e906eddaed90b
Val <valdo_jopa@hotmail.com>
2021-04-09 10:43:25 -0500

    agregar carpeta 23 EJERCCIOS CON DATAFRAMES

---
+++ 23 ejercicios con dataframes/166_ejercicio_1.py
@@ -0,0 +1,8 @@
+# ejerccio  
+
+#vamos a filtar datos en un dataframe
+
+#crearemos una lista de 50 valores aleatorios enteros entre los valores 0 y 100
+#convertiremos esta lista en un dataframe con 5 filas y 10 columnas
+#filtaremos los datos del dataframe para visualizar solo los valores que sean mayores de 50
+
