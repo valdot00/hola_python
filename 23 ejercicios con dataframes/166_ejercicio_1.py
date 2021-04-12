7b7f56d43b9c28e76d1064e96c1ca0b2a0868fe9
Val <valdo_jopa@hotmail.com>
2021-04-12 11:49:14 -0500

    166 ejercicio 1 : actividad completada

--- 23 ejercicios con dataframes/166_ejercicio_1.py
+++ 23 ejercicios con dataframes/166_ejercicio_1.py
@@ -6,3 +6,21 @@
 #convertiremos esta lista en un dataframe con 5 filas y 10 columnas
 #filtaremos los datos del dataframe para visualizar solo los valores que sean mayores de 50
 
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+import numpy as np
+
+lista_valores = np.random.randint(0,100,50)
+
+print(lista_valores)
+
+print(lista_valores.resize(5,10))
+
+print(lista_valores)
+
+dataframe = pd.DataFrame(lista_valores)
+
+print(dataframe)
+
+print(dataframe[dataframe>50])
