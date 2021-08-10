0195b2173d07823f911223b0b3c7b5880ceb8483
Val <valdo_jopa@hotmail.com>
2021-08-10 10:31:32 -0500

    entradas

--- 25 Proyecto final/portada.py
+++ 25 Proyecto final/portada.py
@@ -16,6 +16,25 @@ etiqueta3.grid(row=1,column=0)
 etiqueta4 = Label(ventana, text="ISBN")
 etiqueta4.grid(row=1,column=2)
 
+#entrada de datos
+
+titulo = StringVar()
+entrada1 = Entry(ventana,textvariable=titulo)
+entrada1.grid(row=0,column=1)
+
+autor = StringVar()
+entrada2 = Entry(ventana,textvariable=autor)
+entrada2.grid(row=0,column=3)
+
+year = StringVar()
+entrada3 = Entry(ventana,textvariable=year)
+entrada3.grid(row=1,column=1)
+
+isbn = StringVar()
+entrada4 = Entry(ventana,textvariable=isbn)
+entrada4.grid(row=1,column=3)
+
+#titulo
 ventana.title("Libros")
 ventana.mainloop()
 
