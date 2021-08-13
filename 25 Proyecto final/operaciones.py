21ad7584e2498703f49fcd60d353501a73e9adcc
Val <valdo_jopa@hotmail.com>
2021-08-12 19:58:23 -0500

    funciones actualizar, buscar y borrar

--- 25 Proyecto final/operaciones.py
+++ 25 Proyecto final/operaciones.py
@@ -8,7 +8,6 @@ def conectar():
     conexion.commit()
     conexion.close()
 
-
 def insertar(titulo,autor,year,isbn):
     conexion = sqlite3.connect("libros.db")
     cursor = conexion.cursor()
@@ -22,8 +21,31 @@ def Visualizar():
     cursor.execute("SELECT * FROM libros")
     resultado = cursor.fetchall()
     conexion.close()
-    return resultado    
+    return resultado  
 
+def buscar(titulo,autor,year,isbn):
+    conexion = sqlite3.connect("libros.db")
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM libros WHERE titulo=? OR year=? OR isbn=?",(titulo,autor,year,isbn))
+    resultado = cursor.fetchall()
+    conexion.cloce()
+    return resultado
+
+def borrar(id):
+    conexion = sqlite3.connect("libros.db")
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM libros WHERE id=?",(id))
+    conexion.commit()
+    conexion.close()
+
+def Actualizar(titulo,autor,year,isbn,id):
+    conexion = sqlite3.connect("libros.db")
+    cursor = conexion.cursor()
+    cursor = conexion.cursor()
+    cursor.execute("UPDATE libros SET titulo=?, autor=?, year=?, isbn=?, WHERE id=?",(titulo,autor,year,isbn,id))
+    conexion.commit()
+    conexion.close()
+    
 #pruebas
 conectar()
 insertar("titulo1","autor1",2000,123456789)
